e272fd16cdceadc683d85941476af164af4b2c86
Yichi Zhang <kevincham1993@gmail.com>
2018-04-01 16:25:40 +0800

    update batch script

--- batch_script.py
+++ batch_script.py
@@ -25,6 +25,17 @@ def populate_intervened_posts():
 
     subprocess.run("perl make_noinstructor_corpus.pl -dbname coursera -course eQJvsjn9EeWJaxK5AT4frw -density", shell=True)
 
+def compute_term_weights():
+    """Compute the term weights and insert into the database.
+    Run this method twice in the pipeline to collect TFs and populate it in termfreqc14inst and termfreqc14noinst.
+    """
+
+    subprocess.run("perl compute_term_weights.pl -dbname coursera -course eQJvsjn9EeWJaxK5AT4frw  -uni  -tf -thread inst", shell=True)
+    subprocess.run("perl compute_term_weights.pl -dbname coursera -course eQJvsjn9EeWJaxK5AT4frw  -uni  -tf -thread noinst", shell=True)
+    # This is the second round running these two scripts
+    subprocess.run("perl compute_term_weights.pl -dbname coursera -course eQJvsjn9EeWJaxK5AT4frw  -uni  -tf -thread inst", shell=True)
+    subprocess.run("perl compute_term_weights.pl -dbname coursera -course eQJvsjn9EeWJaxK5AT4frw  -uni  -tf -thread noinst", shell=True)
+
 def update_docid():
     """creates document ids, one per each thread.
     This is useful when threads from all the courses are in the same database.
@@ -32,12 +43,12 @@ def update_docid():
     """
 
     subprocess.run("perl updatedocid.pl -dbname coursera -course eQJvsjn9EeWJaxK5AT4frw", shell=True)
-#
-# def generate_feature():
-#     subprocess.run("perl gen_features.pl -course eQJvsjn9EeWJaxK5AT4frw  -dbname coursera  -uni -allf", shell=True)
-#
-# def classifier_model():
-#     subprocess.run("perl predict_thread_intervention.pl -course eQJvsjn9EeWJaxK5AT4frw -w nve -in uni+forum+affir+tlen+nums+nont_course_train+test_0_all.txt -model <one of the model files>", shell=True)
+
+def generate_feature():
+    subprocess.run("perl gen_features.pl -course eQJvsjn9EeWJaxK5AT4frw  -dbname coursera  -uni -allf", shell=True)
+
+def classifier_model():
+    subprocess.run("perl predict_thread_intervention.pl -course eQJvsjn9EeWJaxK5AT4frw -w nve -in uni+forum+affir+tlen+nums+nont_course_train+test_0_all.txt -model <one of the model files>", shell=True)
 
 if __name__ == '__main__':
     if debug:
